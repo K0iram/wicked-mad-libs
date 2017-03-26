db11b98ceb56f93abf53320722a19331f0917c26
mario7746 <m.kennedykavouras@gmail.com>
2017-03-26 15:46:22 -0400

    initial commit

---
+++ src/layouts/main/index.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+import Navigation from './components/Navigation/'
+import './App.css'
+
+class AppLayout extends Component {
+  render() {
+    return (
+      <div className="App">
+          <h1>Wicked Mad Libs</h1>
+          <Navigation />
+
+        <div className="main-content">
+          {this.props.children}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default AppLayout;
