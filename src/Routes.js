3ec30980b62198c14ad7907b123d28bab7d4c6be
mario7746 <m.kennedykavouras@gmail.com>
2017-03-30 19:46:25 -0400

    declared Switch at top of route file.

--- src/Routes.js
+++ src/Routes.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Router from 'react-router-dom/HashRouter'
 import Route from 'react-router-dom/Route'
-import Switch from 'react-router-dom'
+import Switch from 'react-router-dom/Switch'
 
 
 import AppLayout from './layouts/main'
