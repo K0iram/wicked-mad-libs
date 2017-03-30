5f39cd65f097900c13620c6a1042296af46b8d94
mario7746 <m.kennedykavouras@gmail.com>
2017-03-29 22:11:59 -0400

    changes home template to render on /

--- src/Routes.js
+++ src/Routes.js
@@ -20,6 +20,7 @@ const Routes = () => (
   <Router>
     <AppLayout>
       <Route path="/" component={Home} />
+      <Route path="/home" component={Home} />
       <Route exact path="/stories" component={Stories} />
       <Route exact path="/stories/:storyId" component={Stories} />
       <Route path="/about" component={About} />
